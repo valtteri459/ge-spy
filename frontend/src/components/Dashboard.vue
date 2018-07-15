70b292a4cfbf217b20606239b99a98becee7a9ed
Valtteri <valtteri.valonen@gmail.com>
2018-07-15 18:47:58 +0300

    updated dashboard(?)

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,16 +1,15 @@
 <template>
   <v-container fluid class="page">
     <muuri-grid id="example-grid">
-
-      <item-md height="100px">
-        <v-card><v-card-title><h3>oof1 - </h3></v-card-title><v-card-text>{{priceData.length}}</v-card-text></v-card>
-      </item-md>
-      <item-md>
-        <v-card><v-card-title><h3>oof2</h3></v-card-title></v-card>
-      </item-md>
-      <item-md height="100px">
-        <v-card><v-card-title><h3>oof3</h3></v-card-title></v-card>
-      </item-md>
+      <div id="ooffie" class="item"
+        :style="{
+          'width': '100%',
+          'max-width': '300px'
+        }" v-for="item in [1,2,3,4,5,6,7,8,9,0]" :key="item">
+        <div class="item-content item-md">
+          <v-card><v-card-title><h3>oof - {{item}}</h3></v-card-title><v-card-text>{{priceData.length}}</v-card-text></v-card>
+        </div>
+      </div>
     </muuri-grid>
   </v-container>
 </template>
