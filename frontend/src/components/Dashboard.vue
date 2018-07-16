0bdba295854f6712b1e0cf7ac923615112b081b4
Valtteri <valtteri.valonen@gmail.com>
2018-07-16 07:49:19 +0300

    updated dashboard

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,28 +1,52 @@
 <template>
-  <v-container fluid class='page'>
-<GridLayout
-            :layout="testLayout"
-            :col-num="12"
-            :row-height="30"
-            :is-draggable="true"
-            :is-resizable="false"
-            :vertical-compact="false"
-            :margin="[10, 10]"
-            :use-css-transforms="true"
-    >
-        <GridItem v-for="item in testLayout"
-                   :x="item.x"
-                   :y="item.y"
-                   :w="item.w"
-                   :h="item.h"
-                   :i="item.i"
-                   :key="item.i">
-            <v-card height="100%" class="flexcard">
-              <v-card-text class="grow">{{item.i}}</v-card-text>
-              <v-card-actions><v-btn flat color="orange" @click="testLayout.splice(testLayout.indexOf(item), 1)">delme</v-btn></v-card-actions>
-            </v-card>
-        </GridItem>
+  <v-container fluid class='page' style="padding: 0px">
+    <v-layout>
+      <v-flex xs12>
+        <v-card>
+          <v-card-text>
+            <h2>Personal dashboard
+            <v-btn color="primary" style="float: right" @click="newVidget = true">Add widget</v-btn>
+            </h2>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <GridLayout
+      style="margin:24px"
+      :layout="testLayout"
+      :col-num="12"
+      :row-height="30"
+      :is-draggable="true"
+      :is-resizable="false"
+      :vertical-compact="true"
+      :margin="[10, 10]"
+      :use-css-transforms="true">
+      <GridItem v-for="item in testLayout"
+        :x="item.x"
+        :y="item.y"
+        :w="item.w"
+        :h="item.h"
+        :i="item.i"
+        :key="item.i">
+        <v-card height="100%" class="flexcard">
+         <v-card-text class="grow">{{item.i}}</v-card-text>
+          <v-card-actions><v-btn flat color="orange" @click="testLayout.splice(testLayout.indexOf(item), 1)">delme</v-btn></v-card-actions>
+        </v-card>
+      </GridItem>
     </GridLayout>
+    <v-dialog v-model="newVidget" max-width="500px">
+      <v-card>
+        <v-card-title>
+          Lisää laite
+        </v-card-title>
+        <v-card-text>
+          <v-text-field name='type' label='Laitteen tyyppi' v-model="testoTusti"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary">Tallenna</v-btn><v-spacer></v-spacer><v-btn color="primary" flat @click.stop="newVidget=false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -31,32 +55,14 @@ import grids from 'vue-grid-layout'
 var GridLayout = grids.GridLayout
 var GridItem = grids.GridItem
 export default {
-  name: 'HelloWorld',
+  name: 'Dashboard',
   data () {
     return {
-      source: 'https://noire.io',
       priceData: [],
+      newVidget: false,
+      testoTusti: '',
       testLayout: [
-        {'x': 0, 'y': 0, 'w': 4, 'h': 4, 'i': '0'},
-        {'x': 2, 'y': 0, 'w': 3, 'h': 4, 'i': '1'},
-        {'x': 4, 'y': 0, 'w': 2, 'h': 5, 'i': '2'},
-        {'x': 6, 'y': 0, 'w': 2, 'h': 4, 'i': '3'},
-        {'x': 8, 'y': 0, 'w': 6, 'h': 4, 'i': '4'},
-        {'x': 10, 'y': 0, 'w': 5, 'h': 4, 'i': '5'},
-        {'x': 0, 'y': 5, 'w': 2, 'h': 5, 'i': '6'},
-        {'x': 2, 'y': 5, 'w': 2, 'h': 5, 'i': '7'},
-        {'x': 4, 'y': 5, 'w': 2, 'h': 5, 'i': '8'},
-        {'x': 6, 'y': 4, 'w': 2, 'h': 4, 'i': '9'},
-        {'x': 8, 'y': 4, 'w': 2, 'h': 4, 'i': '10'},
-        {'x': 10, 'y': 4, 'w': 2, 'h': 4, 'i': '11'},
-        {'x': 0, 'y': 10, 'w': 2, 'h': 5, 'i': '12'},
-        {'x': 2, 'y': 10, 'w': 2, 'h': 5, 'i': '13'},
-        {'x': 4, 'y': 8, 'w': 2, 'h': 4, 'i': '14'},
-        {'x': 6, 'y': 8, 'w': 2, 'h': 4, 'i': '15'},
-        {'x': 8, 'y': 10, 'w': 2, 'h': 5, 'i': '16'},
-        {'x': 10, 'y': 4, 'w': 2, 'h': 4, 'i': '17'},
-        {'x': 0, 'y': 9, 'w': 2, 'h': 4, 'i': '18'},
-        {'x': 2, 'y': 6, 'w': 2, 'h': 8, 'i': '19'}
+        {'x': 0, 'y': 0, 'w': 12, 'h': 4, 'i': '0'}
       ]
     }
   },
