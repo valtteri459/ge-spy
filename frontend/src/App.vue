93d625c0a0913772cbd9f0e10e74b62656ade004
Valtteri <valtteri.valonen@gmail.com>
2022-02-24 16:05:59 +0200

    base vue3 composition template

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+  
+  const count = ref(0)
+  onMounted(() => {
+    console.log(count.value) // 0
+  })
+</script>
+
+<template>
+  <button @click="count++">{{ count }}</button>
+</template>
