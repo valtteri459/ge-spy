2d460a34e4b0700ee4291be1b74bb1ed1dd4f2d2
Valtteri <valtteri.valonen@gmail.com>
2023-11-10 21:22:06 +0200

    init new stuff

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      model-value
+      class="pt-4"
+    >
+      <v-list>
+        <v-list-item
+          prepend-icon="mdi-account-alert"
+          title="Guest user"
+          subtitle="Log in"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
